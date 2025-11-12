ad64e672f5cce2387e49e599583bbbfed8b7a4ba
Morten Enemark Lund <melund@gmail.com>
2025-11-12 15:55:43 +0100

    Update script and add github action

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 
-import polars as pl
-from anypytools import AnyPyProcess
+import glob
+import itertools
+import os
+
 import anypytools.macro_commands as mc
 import numpy as np
+import polars as pl
+import typer
+from anypytools import AnyPyProcess
 
+app = typer.Typer(no_args_is_help=True, add_completion=False)
 
 # Maps the DOF name to its description/axis-label and sign convention compared to AMS
 dof_map = {
@@ -74,62 +80,106 @@ rom_secondary_dof = {
 
 MUSCLE_TYPES = ["Simple", "3E_1Par", "Strong3E_2Par", "3E_Experimental"]
 
-## Save calibration files for all muscle models
-calibration_macros = []
-for muscle_type in MUSCLE_TYPES:
-    calibration_macros.extend([
-        mc.Load("EvaluateJointStrength.main.any"),
-        mc.OperationRun("Main.HumanModel.Calibration.CalibrationSequence"),
-        mc.SaveValues(f"{muscle_type}_calibration.anyset")
-    ])
-app = AnyPyProcess(num_processes=4)
-results = app.start_macro(calibration_macros)
-assert all("ERROR" not in results)
+
+def generate_muscle_calibration_macros() -> list[list[mc.MacroCommand]]:
+    ## Save calibration files for all muscle models
+    calibration_macros = []
+    for muscle_type in MUSCLE_TYPES:
+        calibration_macros.append([
+            mc.Load("EvaluateJointStrength.main.any"),
+            mc.OperationRun("Main.HumanModel.Calibration.CalibrationSequence"),
+            mc.SaveValues(f"{muscle_type}_calibration.anyset")
+        ])
+    return calibration_macros
 
 
 
-macros = []
 
-for muscle_type in MUSCLE_TYPES:
+def generate_muscle_simulation_macros()-> list[list[mc.MacroCommand]]:
+    macros = []
+
     for study, (primary_dof, secondary_dof) in simulation_plan.items():
-        rom_primary = range_of_motion[study]
-        submacros = []
-        rom_secondary = np.linspace(*rom_secondary_dof[secondary_dof], 40)
-        for secondary_val in rom_secondary:
-            submacros.append([
-                mc.Load("EvaluateJointStrength.main.any"),
-                # Load the calibration for the given muscle model
-                mc.LoadValues(f"{muscle_type}_calibration.anyset"),
-                mc.UpdateValues(),
-                # Set parameters for easier output
-                mc.SetValue(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.RangeOfMotion", rom_primary ),
-                mc.SetValue(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.{secondary_dof}", secondary_val),
-                # Run the inverse dynamics study
-                mc.OperationRun(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.Study.InverseDynamics"),
-                # Export results
-                mc.Export(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.Study.Output.JointStrength.Abscissa.JointAngle", "measurePrimaryDoF"),
-                mc.Export(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.Study.Output.JointStrength.JointStrength", "measureValue"),
-                mc.ExtendOutput("AnyBodyMuscleType", muscle_type),
-                mc.ExtendOutput("measureObject", study_map[study]),
-                mc.ExtendOutput("primaryDoF", dof_map[primary_dof][0]),
-                mc.ExtendOutput("secondaryDoF", dof_map[secondary_dof][0]),
-                mc.ExtendOutput("measureSecondDoF", secondary_val),
-                mc.ExtendOutput("measureSecondDoFSign", dof_map[secondary_dof][1]),
-                mc.ExtendOutput("measurePrimaryDoFSign", dof_map[primary_dof][1]),
-            ])
-        macros.extend(submacros)
+        for muscle_type in MUSCLE_TYPES:
+            rom_primary = range_of_motion[study]
+            submacros = []
+            rom_secondary = np.linspace(*rom_secondary_dof[secondary_dof], 40)
+            for secondary_val in rom_secondary:
+                submacros.append([
+                    mc.Load("EvaluateJointStrength.main.any"),
+                    # Load the calibration for the given muscle model
+                    mc.LoadValues(f"{muscle_type}_calibration.anyset"),
+                    mc.UpdateValues(),
+                    # Set parameters for easier output
+                    mc.SetValue(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.RangeOfMotion", rom_primary ),
+                    mc.SetValue(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.{secondary_dof}", secondary_val),
+                    # Run the inverse dynamics study
+                    mc.OperationRun(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.Study.InverseDynamics"),
+                    # Export results
+                    mc.Export(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.Study.Output.JointStrength.Abscissa.JointAngle", "measurePrimaryDoF"),
+                    mc.Export(f"Main.HumanModel.EvaluateJointStrength.Right.Leg.{study}.Study.Output.JointStrength.JointStrength", "measureValue"),
+                    mc.Export("Main.AMMRGitBranch", "ammr_git_branch"),
+                    mc.Export("Main.AMMRGitHash", "ammr_git_hash"),
+                    mc.Export("Main.AnyBodyVersion", "anybody_version"),
+                    mc.ExtendOutput("AnyBodyMuscleType", muscle_type),
+                    mc.ExtendOutput("measureObject", study_map[study]),
+                    mc.ExtendOutput("primaryDoF", dof_map[primary_dof][0]),
+                    mc.ExtendOutput("secondaryDoF", dof_map[secondary_dof][0]),
+                    mc.ExtendOutput("measureSecondDoF", secondary_val),
+                    # Add sign conventions, so we can later correct the output to mactch strength dataset from the publication
+                    mc.ExtendOutput("measureSecondDoFSign", dof_map[secondary_dof][1]),
+                    mc.ExtendOutput("measurePrimaryDoFSign", dof_map[primary_dof][1]),
+                ])
+            macros.extend(submacros)
+    return macros
 
-app = AnyPyProcess(num_processes=5)
-results = app.start_macro(macros)
+@app.command()
+def batch_process(batch: int|None = None, num_batches: int|None = None):
 
-df_pandas = results.to_dataframe(index_var="measurePrimaryDoF")
+    # app = AnyPyProcess()
+    # cal_macros = generate_muscle_calibration_macros()
+    # print("Running calibration macros...")
+    # results = app.start_macro(cal_macros)
+    # assert "ERROR" not in results, f"Failed with: {results['ERROR']}"
 
-# Change signs of variables which are opposite to the AnyBody convention
-df = pl.from_pandas(df_pandas).with_columns(
-    pl.col("measureSecondDoF")*pl.col("measureSecondDoFSign").alias("measureSecondDoF"),
-    pl.col("measurePrimaryDoF")*pl.col("measurePrimaryDoFSign").alias("measurePrimaryDoF"),
-).drop(["measureSecondDoFSign", "measurePrimaryDoFSign"])
+    macros = generate_muscle_simulation_macros()
 
-df.write_parquet("joint_strength_results.parquet")
+    if num_batches and num_batches > len(macros):
+        raise ValueError("number_of_batches is larger than the number of macros")
 
-print(df)
+    if batch is not None and num_batches:
+        batch_len = (len(macros) + num_batches - 1) // num_batches
+        macros = list(list(itertools.batched(macros, batch_len))[batch-1])
+
+    app = AnyPyProcess(num_processes=5)
+    print("Running strength evaluation macros...")
+    results = app.start_macro(macros)
+    assert "ERROR" not in results, f"Failed with: {results['ERROR']}"
+
+    df_pandas = results.to_dataframe(index_var="measurePrimaryDoF")
+
+    # Change signs of variables which are opposite to the AnyBody convention
+    df = pl.from_pandas(df_pandas).with_columns(
+        pl.col("measureSecondDoF")*pl.col("measureSecondDoFSign").alias("measureSecondDoF"),
+        pl.col("measurePrimaryDoF")*pl.col("measurePrimaryDoFSign").alias("measurePrimaryDoF"),
+    ).drop(["measureSecondDoFSign", "measurePrimaryDoFSign"])
+
+    batch_label = f"{batch}" if batch else ""
+    df.write_parquet(f"joint_strength_results_{batch_label}.parquet")
+
+number_of_batches = 100
+
+@app.command()
+def cleanup_parquet_files():
+    for fn in glob.glob("joint_strength_*.parquet"):
+        os.remove(fn)
+
+
+@app.command()
+def combine_parquet_files(input_pattern: str = "joint_strength_*.parquet", output: str = "joint_strength.parquet"):
+    # concatenate all parquet files file
+    pl.scan_parquet(input_pattern).sink_parquet(output)
+
+
+if __name__ == "__main__":
+    app()
+
