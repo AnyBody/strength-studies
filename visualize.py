53d76b24142049990aa561e77b22ab15ae96c308
Morten Enemark Lund <melund@gmail.com>
2026-01-22 10:23:18 +0100

    Update code to get data from cloud

--- visualize.py
+++ visualize.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.18.0"
+__generated_with = "0.19.4"
 app = marimo.App(
     width="medium",
     app_title="Strength Viewer",
@@ -10,7 +10,9 @@ app = marimo.App(
 
 @app.cell(hide_code=True)
 def _(df, fig, mo, ui_selected_second_dof, ui_selected_study):
-    mo.vstack([ui_selected_study,ui_selected_second_dof, mo.ui.plotly(fig), mo.ui.table(df, page_size=5)])
+    mo.vstack(
+        [ui_selected_study, ui_selected_second_dof, mo.ui.plotly(fig), mo.ui.table(df, page_size=5)]
+    )
     return
 
 
@@ -21,10 +23,27 @@ def _():
     import plotly.graph_objects as go
     import polars as pd
     import polars as pl
+    return mo, pd, pl, px
 
-    df_full = pd.read_parquet("joint-strength-full.parquet")
-    #df_full
-    return df_full, mo, pd, pl, px
+
+@app.cell
+def _(pl):
+    source = "abfs://strength-data@anybodydatasets.dfs.core.windows.net/"
+    storage_options = {
+        "sas_token": r"sp=r&st=2026-01-22T09:04:53Z&se=2028-08-10T16:19:53Z&spr=https&sv=2024-11-04&sr=c&sig=DSoRzE2CnJShYZmVs%2BhbWXPSyVk36XnNmdqswjtkFBI%3D"
+    }
+
+
+    def get_data():
+        df = pl.scan_parquet(
+            source + f"*.parquet", storage_options=storage_options, include_file_paths="file"
+        ).filter(pl.col("file") == pl.col("file").max())
+        return df.collect()
+
+
+    df_full = get_data()
+    # df_full
+    return (df_full,)
 
 
 @app.cell
@@ -42,12 +61,12 @@ def _(df_study, mo):
     ui_selected_second_dof = mo.ui.dropdown(
         options=df_study["measureSecondDoF"].unique().sort().to_list(),
         value=0,
-        label=f"Second DOF: {df_study["secondaryDoF"].first()}",
+        label=f"Second DOF: {df_study['secondaryDoF'].first()}",
     )
     ui_selected_second_dof = mo.ui.dropdown(
         options=df_study["measureSecondDoF"].unique().sort().to_list(),
         value=0,
-        label=f"Second DOF: {df_study["secondaryDoF"].first()}",
+        label=f"Second DOF: {df_study['secondaryDoF'].first()}",
     )
     return (ui_selected_second_dof,)
 
@@ -61,7 +80,7 @@ def _(df_full, pl, ui_selected_study):
 @app.cell
 def _(df_study, pl, ui_selected_second_dof):
     df = df_study.filter(pl.col("measureSecondDoF") == ui_selected_second_dof.value)
-    #df
+    # df
     return (df,)
 
 
@@ -73,14 +92,13 @@ def _(df, px):
         xaxis_title=df["primaryDoF"].unique()[0],
         yaxis_title="Strength",
         title=f"{df['measureObject'].first()} Strength",
-    );
+    )
     return (fig,)
 
 
 @app.cell
 def _(df_measured_all):
     df_measured_all
-
     return
 
 
@@ -91,16 +109,16 @@ def _(mo, pd):
         options=df_measured_all["measureObject"].unique().sort().to_list(),
         value=df_measured_all["measureObject"].first(),
         label="Select Joint Measurement:",
-    );
-    #ui_selected_measured
-    #df_measured_all
+    )
+    # ui_selected_measured
+    # df_measured_all
     return df_measured_all, ui_selected_measured
 
 
 @app.cell
 def _(df_measured_all, pl, ui_selected_measured):
-    df_measured = df_measured_all.filter(pl.col("measureObject")==ui_selected_measured.value)
-    #df_measured
+    df_measured = df_measured_all.filter(pl.col("measureObject") == ui_selected_measured.value)
+    # df_measured
     return
 
 
